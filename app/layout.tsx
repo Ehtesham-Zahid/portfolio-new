efcfa499966b3fd7854662607f3f92d16eb2f0e4
Ehtesham-Zahid <shaimalick321@gmail.com>
2024-09-04 02:50:31 +0500

    Analytics Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
@@ -29,6 +30,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
